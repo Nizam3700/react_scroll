9e274951ff244f1477385037685a560215957941
Nizamuddin <nizamuddin8637@gmail.com>
2024-07-25 13:16:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import {useRef} from "react";
 import './App.css';
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
+  const services = useRef(null);
+  const blog = useRef(null);
+  const contacts = useRef(null);
+
+  const scrollToSection = (elementRef) =>{
+    
+    window.scrollTo({
+      top: elementRef.current.offsetTop,
+      behavior: "smooth",
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ScrollToTop/>
+     <div className='hero'>
+      <ul>
+        <li onClick={() => scrollToSection(services)} className='link'>Services</li>
+        <li onClick={() => scrollToSection(blog)} className='link'>Blog</li>
+        <li onClick={() => scrollToSection(contacts)} className='link'>Contact</li>
+      </ul>
+     </div>
+     <div ref={services} className='services'>
+      <h3>Services</h3>
+     </div>
+     <div ref={blog} className='blog'>
+      <h3>Blog</h3>
+     </div>
+     <div ref={contacts} className='contacts'>
+      <h3>Contacts</h3>
+     </div>
     </div>
   );
 }
---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react'
+import { FaAngleDoubleUp } from "react-icons/fa";
+
+const ScrollToTop = () => {
+    const [showScrollTopButton, setShowScrollTopButton] = useState(false);
+
+    
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 300){
+                setShowScrollTopButton(true);
+            }else{
+                setShowScrollTopButton(false);
+            }
+        })
+    }, []);
+
+    const scrollTop = (elementRef) =>{
+        window.scrollTo({
+          top:0,
+          behavior: "smooth",
+        })
+      }
+
+
+  return (
+    <>
+    {showScrollTopButton && <FaAngleDoubleUp className="top-btn-position" onClick={scrollTop}/>}
+    </>
+  )
+}
+
+export default ScrollToTop
